3e73aa6ee1c54b68bdd26d5eba9e7d9cf18c15ac
alex@aml.codes <alex@aml.codes>
2020-07-09 22:34:21 -0400

    update with es6 imports and toren fix

--- src/commands/list-members.js
+++ src/commands/list-members.js
@@ -1,33 +1,34 @@
-const logger = require('../logger');
-const state = require('../state');
+import State from '../state'
 
 class ListMembers {
   constructor(memberListRepository) {
-    this.memberList = memberListRepository;
-    this.commandString = '/list';
+    this.memberList = memberListRepository
+    this.commandString = '/list'
   }
 
   shouldExecute(messageService) {
     if (messageService.getCommand() === this.commandString) {
-      return true;
+      return true
     }
-    return false;
+    return false
   }
 
   execute(messageService) {
-    const members = this.memberList.getMemberList();
-    let reply;
+    const members = this.memberList.getMemberList()
+    let reply
     if (members === undefined || members.length === 0) {
       reply = 'List of members is currently empty'
     } else {
       const userList = members.join('\n')
       reply = `Current members:\n${userList}`
-      let userString = '';
+      let userString = ''
       for (let i = 0; i < members.length - 1; i += 1) {
-        userString += `${members[i].userID}, ${members[i].alias}\n`;
+        userString += `${members[i].userID}, ${members[i].alias}\n`
       }
-      userString += `${members[members.length - 1].userID}, ${members[members.length - 1].alias}`;
-      reply = `Current members:\n${userString}`;
+      userString += `${members[members.length - 1].userID}, ${
+        members[members.length - 1].alias
+      }`
+      reply = `Current members:\n${userString}`
     }
     const obj = {
       reply,
