d96178d49434024ce3965caca93adcefd25cfbf7
alex@aml.codes <alex@aml.codes>
2020-07-21 10:23:08 -0400

    readd jest test

---
+++ src/__tests__/sample.test.js
@@ -0,0 +1,8 @@
+// import request from 'supertest'
+// import startServer from '../api/server'
+
+describe('proxy bot', () => {
+  test('It should proxy', async () => {
+    expect(1).toEqual(1)
+  })
+})
