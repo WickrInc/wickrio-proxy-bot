3d7e508d0d308780813220a03c44ca216f2ac81c
toren-wickr <tderosa@wickr.com>
2020-09-02 11:57:09 -0400

    Adding remove asset and remove member commands to the help

--- src/commands/help.js
+++ src/commands/help.js
@@ -14,8 +14,9 @@ class Help {
       '/asset <asset-name>: This will create a new room with the group of users posting your question\n' +
       '/create : This will create a room with the group of users, messages sent to the room will be sent to the asset\n' +
       '/add <member> <alias>: This will add a member with an alias to the group\n' +
-      // '/remove <members> : This will remove a comma separated list of users from the group\n' +
-      '/send <message> : This will send <message> to the asset in a one-to-one with the proxy bot\n' +
+      '/remove <member> : This will remove a member from the list of users\n' +
+      '/delete <asset> : This will delete an asset from the list of assets\n' +
+      '/send <message> : This will send <message> to the asset in a one-to-one with the proxy bot. Must be in the created room to send\n' +
       '/list : This will return the list of users currently in the group\n\n' +
       '*Admin Commands*\n' +
       '/admin list : Get list of admin users \n' +
@@ -25,8 +26,6 @@ class Help {
       '/help : Show help information\n' +
       '/version : Get the version of the integration\n' +
       '/cancel : To cancel the last operation and enter a new command\n'
-    // '/files : To get a list of saved files that contain rooms'
-    // var sMessage = WickrIOAPI.cmdSendRoomMessage(vGroupID, reply);
     return {
       reply,
       state: State.NONE,
