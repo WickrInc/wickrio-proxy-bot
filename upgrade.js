a0dc9926dcf8f6467876bc9d1898971f9b6f0c83
Greg Messina <gregmessina@MacBook-Pro.fios-router.home>
2020-09-28 18:21:02 -0400

    removing node from the script

--- upgrade.js
+++ upgrade.js
@@ -8,5 +8,5 @@ const Proc = require('./processes.json')
 const fs = require('fs')
 
 var dataToChange = JSON.parse(fs.readFileSync('./processes.json', 'utf-8'))
-dataToChange.apps[0].script = 'node ./build/index.js'
+dataToChange.apps[0].script = './build/index.js'
 fs.writeFileSync('./processes.json', JSON.stringify(dataToChange, null, 2))
