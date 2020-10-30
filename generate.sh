f2bb3d4eeef27c03de676d37ea416d55e3c2f719
toren-wickr <54855836+toren-wickr@users.noreply.github.com>
2020-10-30 14:17:03 -0400

    Toren config tokens (#25)
    * WIP: Proxy service with empty credential file
    
    * WIP: Fixing proxy service with full cred file
    
    * About to pull master
    
    * update
    
    * getUserInfo error
    
    * Send and add rooms with multiple asssets
    
    * Fixing the json-credential-handler
    
    * Bumping version number
    
    * Bumping bot-api version number
    
    * Fixing reply from asset to include username
    
    * WIP: Fixing create command in proxy service
    
    * Fixing add empty asset bug
    
    * Adding the cancel command
    
    * Changing the wording of error message
    
    * Minor fixes to get remove asset to work
    
    * Adding remove asset and remove member commands to the help
    
    * Updating the help command to make steps more clear
    
    * Adding definitions to the help output
    
    * Fixing cancel definition in help command
    
    * No reply for communication in created rooms
    
    * Updating help message and fixing messages in asset room
    
    * Adding configTokens.json file to upgrade script
    
    * Removing configTokens.json from upgrade.sh
    
    * Adding configTokens.json file to generate script
    
    * Alphabetical order in generate.sh
    
    Co-authored-by: alex@aml.codes <alex@aml.codes>
    Co-authored-by: Greg Messina <gregmessina@MacBook-Pro.local>

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ fi
 
 mkdir -p temp
 
-cp -r configure.js configure.sh install.sh package.json processes.json README.md restart.sh src start.sh stop.sh upgrade.js upgrade.sh temp
+cp -r configTokens.json configure.js configure.sh install.sh package.json processes.json README.md restart.sh src start.sh stop.sh upgrade.js upgrade.sh temp
 
 cd temp
 tar czf $1/software.tar.gz *
