e38519771282e99fa3a26f7363039a8b177244d0
toren <tderosa@wickr.com>
2021-01-18 14:51:56 -0500

    Adding state for setup asset, fixing welcome message logic

--- src/state.js
+++ src/state.js
@@ -5,6 +5,7 @@ export default Object.freeze({
   WHICH_ASSET: 1,
   WHICH_ROOM: 2,
   SETUP_ALIAS: 3,
-  CREATE_ROOM_SETUP: 4,
-  WHICH_ROOM_SETUP: 5,
+  SETUP_ASSET: 4,
+  CREATE_ROOM_SETUP: 5,
+  WHICH_ROOM_SETUP: 6,
 })
