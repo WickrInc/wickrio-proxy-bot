389d997253d9a88684ed18233e1aa5823d369d35
toren-wickr <54855836+toren-wickr@users.noreply.github.com>
2022-05-10 09:20:52 -0400

    Fixed logging to use latest bot-api logger (#48) (#50)
    * Fixed logging to use latest bot-api logger
    
    * Updating version number of proxy-bot
    
    Co-authored-by: toren-wickr <rtoren@amazon.com>
    
    Co-authored-by: toren-wickr <rtoren@amazon.com>

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,11 +1,11 @@
-import { WickrLogger } from 'wickrio-bot-api'
 const util = require('util')
 
-const logger = new WickrLogger().getLogger()
+const logger = require('wickrio-bot-api').logger
 
 console.log = function () {
   logger.info(util.format.apply(null, arguments))
 }
+
 console.error = function () {
   logger.error(util.format.apply(null, arguments))
 }
