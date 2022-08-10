61a60190f207ad97233306dc2be9ac46fbfb936b
Paul Cushman <paulcus@amazon.com>
2022-08-10 13:23:47 -0700

    upgrade to node 16

--- ent-sync.sh
+++ ent-sync.sh
@@ -1,2 +1,7 @@
+if [ -f "/usr/local/nvm/nvm.sh" ]; then
+  . /usr/local/nvm/nvm.sh
+  nvm use 16
+fi
+
 npm run transpile
 rsync -r --exclude-from 'exclude-list.txt' . centos@172.22.36.163:/home/centos/containers/toren-ent-proxy-bot/clients/torenproxy_fdr8-gorilla.dev.secmv.net/integration/wickrio-broadcast-bot/
