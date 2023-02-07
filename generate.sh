a5232760ff7e6d157a4094ecfc7fe93cdd9c6306
toren-wickr <54855836+toren-wickr@users.noreply.github.com>
2023-02-07 14:16:59 -0500

    Replacing forever with WPM2 (#52)
    Co-authored-by: toren-wickr <rtoren@amazon.com>

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ fi
 
 mkdir -p temp
 
-cp -r configTokens.json configure.js configure.sh .foreverignore forever.json install.sh LICENSE package.json processes.json README.md restart.sh src start.sh stop.sh upgrade.js upgrade.sh temp
+cp -r configTokens.json configure.js configure.sh install.sh LICENSE package.json processes.json README.md restart.sh src start.sh stop.sh upgrade.js upgrade.sh wpm.json temp
 
 cd temp
 tar czf $1/software.tar.gz *
